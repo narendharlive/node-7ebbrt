c1e3544cef5075ff9c496a5ca7f19905fbce13d6
narendharlive <narendharlive@gmail.com>
2021-07-18 14:28:20 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.sendFile('./index.html');
 });
 
 server.listen(port, hostname, () => {

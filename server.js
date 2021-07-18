cf7725326218580f0c7b18e8757341b7958848e8
narendharlive <narendharlive@gmail.com>
2021-07-18 14:38:44 +0530

    1 file modified

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.send('./index.html');
+  res.end('./index.html');
 });
 
 server.listen(port, hostname, () => {

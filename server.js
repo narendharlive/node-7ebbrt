b8663b06dbde79b680212880f041451e0c84c844
narendharlive <narendharlive@gmail.com>
2021-07-18 14:40:37 +0530

    1 file modified

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
 const port = 80;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('./index.html');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+fs.readFile('./index.html', function(err, html) {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer(function(request, response) {
+      response.writeHeader(200, { 'Content-Type': 'text/html' });
+      response.write(html);
+      response.end();
+    })
+    .listen(port);
 });
